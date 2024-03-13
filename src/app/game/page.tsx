0176e90f927e367a4d18dd7e3efc1e00ec616ff7
sshrik <chj878194@naver.com>
2024-03-14 00:46:33 +0900

    feat: use hook at page

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,4 +1,8 @@
+import { useMazeMap } from "@/hooks/useMazeMap";
+
 export default function GamePage() {
+	const maze = useMazeMap(100, 100);
+
 	return (
 		<div className="size-full flex flex-col items-center justify-center gap-4">
 			<h1 className="text-4xl">게임 화면</h1>
