4fe9d77aec2ca486dd60aa567b15a4e6d0521dab
sshrik <chj878194@naver.com>
2024-03-14 00:10:34 +0900

    feat: add pages

---
+++ src/app/game/page.tsx
@@ -0,0 +1,7 @@
+export default function GamePage() {
+	return (
+		<div className="size-full flex flex-col items-center justify-center gap-4">
+			<h1 className="text-4xl">게임 화면</h1>
+		</div>
+	);
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
 	return (
-		<div className="">
-			<Button>Button</Button>
+		<div className="size-full flex flex-col items-center justify-center gap-4">
+			<h1 className="text-4xl">미로 게임</h1>
+			<Button asChild>
+				<Link className="text-sm" href="/game">
+					Game Start
+				</Link>
+			</Button>
 		</div>
 	);
 }
