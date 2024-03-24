db3ef52c1e2a2a26ad0fdc8a11554caa03321a2a
sshrik <chj878194@naver.com>
2024-03-24 14:37:31 +0900

    feat(hook): fix start / end can same problem

--- src/widgets/map/libs/useCreateMaze.ts
+++ src/widgets/map/libs/useCreateMaze.ts
@@ -14,13 +14,15 @@ const useCreateMaze = () =>
 			let row = Math.floor(Math.random() * height);
 			let col = Math.floor(Math.random() * width);
 
-			while (maze[row * 2 + 1][col * 2 + 1] === WALL) {
+			while (maze[row * 2 + 1][col * 2 + 1] !== ROAD) {
 				row = Math.floor(Math.random() * height);
 				col = Math.floor(Math.random() * width);
 			}
 
 			return { row, col };
 		};
+
+		// fill maze`s wall
 
 		for (let h = 0; h < height; h++) {
 			for (let w = 0; w < width; w++) {
@@ -72,10 +74,24 @@ const useCreateMaze = () =>
 			}
 		}
 
-		const start = pickRandomCell();
-		const end = pickRandomCell();
+		// fill maze`s around wall
 
+		for (let h = 0; h < height; h++) {
+			maze[h * 2][0] = WALL;
+			maze[h * 2][width * 2] = WALL;
+		}
+
+		for (let w = 0; w < width; w++) {
+			maze[0][w * 2] = WALL;
+			maze[height * 2][w * 2] = WALL;
+		}
+
+		// pick start and end
+
+		const start = pickRandomCell();
 		maze[start.row * 2 + 1][start.col * 2 + 1] = START;
+
+		const end = pickRandomCell();
 
 		maze[end.row * 2 + 1][end.col * 2 + 1] = END;
 
