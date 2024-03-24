977c560cf50a824e198b46c75dd85427d0776a14
sshrik <chj878194@naver.com>
2024-03-24 15:01:38 +0900

    test(hook): fix duplicated code and typo

--- src/widgets/map/libs/useCreateMaze.test.ts
+++ src/widgets/map/libs/useCreateMaze.test.ts
@@ -2,32 +2,24 @@ import { useCreateMaze } from "@/widgets/map/libs";
 import { END, START } from "@/widgets/map/model";
 import { renderHook } from "@testing-library/react";
 
-describe("useCreateMze", () => {
+describe("useCreateMaze", () => {
 	const WIDTH = 5;
 	const HEIGHT = 5;
 
+	const { result } = renderHook(() => useCreateMaze());
+
+	const maze = result.current(WIDTH, HEIGHT);
+
 	it("should create a maze", () => {
-		const { result } = renderHook(() => useCreateMaze());
-
-		const maze = result.current(WIDTH, HEIGHT);
-
 		expect(maze).toBeDefined();
 	});
 
 	it("should create a maze with the correct size", () => {
-		const { result } = renderHook(() => useCreateMaze());
-
-		const maze = result.current(WIDTH, HEIGHT);
-
 		expect(maze.length).toBe(HEIGHT * 2 + 1);
 		expect(maze[0].length).toBe(WIDTH * 2 + 1);
 	});
 
 	it("should create a maze with the correct start and end", () => {
-		const { result } = renderHook(() => useCreateMaze());
-
-		const maze = result.current(WIDTH, HEIGHT);
-
 		let start = false;
 		let end = false;
 
