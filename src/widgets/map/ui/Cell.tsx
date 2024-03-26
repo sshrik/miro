d078fed40a3869223d636e276b4f2312632a7ad9
sshrik <chj878194@naver.com>
2024-03-27 00:02:38 +0900

    fix: fix cell enum

--- src/widgets/map/ui/Cell.tsx
+++ src/widgets/map/ui/Cell.tsx
@@ -1,4 +1,4 @@
-import { type CellType, END, ROAD, START, WALL } from "@/widgets/map/model";
+import { CellEnum, type CellType } from "@/widgets/map/model";
 
 type CellProps = {
 	type: CellType;
@@ -8,13 +8,13 @@ const Cell: React.FC<CellProps> = (props) => {
 	const { type } = props;
 
 	switch (type) {
-		case ROAD:
+		case CellEnum.ROAD:
 			return <div className="w-8 h-8 bg-white" />;
-		case WALL:
+		case CellEnum.WALL:
 			return <div className="w-8 h-8 bg-gray-500" />;
-		case START:
+		case CellEnum.START:
 			return <div className="w-8 h-8 bg-blue-500" />;
-		case END:
+		case CellEnum.END:
 			return <div className="w-8 h-8 bg-red-500" />;
 		default:
 			return null;
